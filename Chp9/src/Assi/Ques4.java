bf50d01cca8657e7d7e3d993eb3113db2e630cc7
gurav <gurav@SWAYAM>
2025-05-29 12:49:13 +0530

    9th chapter

--- Chp9/src/Assi/Ques4.java
+++ Chp9/src/Assi/Ques4.java
@@ -1,19 +1,5 @@
 package Assi;
 
-class Outer{
-	private static int a = 20;
-	
-	static class Inner {
-		static void innerMeth() {
-			System.out.println(a);
-		}
-	}
-}
 public class Ques4 {
-      public static void main(String[] args) {
-		Outer.Inner.innerMeth();
-		Assi.Outer.Inner.innerMeth();
-	}
+
 }
-
-
--- Chp9/src/Assi/Ques5.java
+++ Chp9/src/Assi/Ques5.java
@@ -1,25 +1,5 @@
 package Assi;
-class Viper{
-	
-
-	public void viperMeth() {
-		class Inner {
-			void innerMeth() {
-				System.out.println("local inner class");
-			}
-		}
-			Inner inner = new Inner();
-			inner.innerMeth();
-		
-	}
-	
-	}
 
 public class Ques5 {
-  public static void main(String[] args) {
-	Viper outer = new Viper();
-	outer.viperMeth();
-	
-}
-}
 
+}
